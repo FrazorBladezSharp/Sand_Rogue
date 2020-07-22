bec459dbedf368095337a35332f797116874a97e
Frazor Sharp <frazor.sharp@virginmedia.com>
2020-07-22 12:46:13 +0100

    Initial includes and typedefs

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
-#include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+/*
+###############################################################
+# Project created by Frazor Sharp 2020 Jul 22
+#
+# Sand_Rogue for C / C++ Version 0.1
+#
+# Contact email         : frazor.sharp@virginmedia.com
+# Github                : https://github.com/FrazorBladezSharp/Sand_Rogue
+# Twitch                : FrazorBladezSharp
+# Youtube               : Frazor Sharp
+#
+# All code is free to use as you please
+# free for all Licence !
+###############################################################
+*/
+
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <cglm/cglm.h>
+/* assimp include files. These three are usually needed. */
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+typedef uint8_t		u8;
+typedef uint32_t	u32;
+typedef uint64_t	u64;
+typedef int8_t		i8;
+typedef int32_t		i32;
+typedef int64_t		i64;
+
+int
+/// The run time entry point for Sand_Rogue
+/// \return 0 = all ok.
+main() {
+    printf("Welcome to Sand_Rogue !\n\n");
+
+    // how are we going to treat our objects
+    // ECS = Entity, Components, System.
+
     return 0;
 }
+
+
